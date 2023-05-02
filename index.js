5a2038d2068df7216bc9707753def44f51618b34
onlyyasad <onlyyasad@gmail.com>
2023-05-02 15:58:25 +0600

    dynamic data send link created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 const chefs = require('./data/chefs.json');
 const recipes = require('./data/recipes.json');
 const cors = require('cors');
@@ -11,6 +12,24 @@ app.get('/', (req, res) =>{
     res.send("Hello From My Server")
 });
 
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
+});
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('i need data for id: ', id);
+    const chefsRecipes = recipes.filter(r => r.chef_id === id) || {};
+    res.send(chefsRecipes);
+});
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('i need data for id: ', id);
+    const singleRecipe = recipes.find(r => r.id === id) || {};
+    res.send(singleRecipe);
+});
+
 app.listen(port, () =>{
-    console.log(`This app is runnin on port ${port}`)
-})
+    console.log(`This app is running on port ${port}`)
+});
