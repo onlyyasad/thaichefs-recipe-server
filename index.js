bd7e14aab50f6f6a83371ec7ef4782a7a3a6bbad
onlyyasad <onlyyasad@gmail.com>
2023-05-02 15:37:17 +0600

    Cors installed and initialized

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) =>{
-    res.send("Hello From My first Server")
+    res.send("Hello From My Server")
 });
 
 app.listen(port, () =>{
